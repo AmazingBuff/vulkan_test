19c90ff55b5712f6ef36d545709770a879af193d
刘浩琪 <lhq499445413@163.com>
2024-12-13 23:14:46 +0800

    add lib

--- src/rendering/resources/render_pass/render_pass_manager.cpp
+++ src/rendering/resources/render_pass/render_pass_manager.cpp
@@ -2,14 +2,27 @@
 #include "rendering/utils/util.h"
 #include "rendering/rhi/vulkan/trans/structure_trans.h"
 #include "base/enum_string/enum_to_string.h"
+
+#define YAML_CPP_STATIC_DEFINE
 #include <yaml-cpp/yaml.h>
+
 #include <rfl.hpp>
+#include <rfl/yaml.hpp>
 
 #include "rendering/rhi/vulkan/trans/enum_trans.h"
 #include "rendering/rhi/vulkan/trans/enum_map.h"
 
 ENGINE_NAMESPACE_BEGIN
 #define RENDER_PASS_PATH SOURCES_DIR"/res/render_passes/"
+
+
+struct Person 
+{
+	rfl::Rename<"firstName", std::string> first_name;
+	rfl::Rename<"lastName", std::string> last_name;
+	rfl::Timestamp<"%Y-%m-%d"> birthday;
+	std::vector<Person> children;
+};
 
 void RenderPassManager::initialize()
 {
@@ -42,7 +55,7 @@ void RenderPassManager::load_render_pass_files()
 
 		const auto& d = config["render_pass"]["attachments"]["descriptions"];
 
-
+		const rfl::Result<Person> result = rfl::yaml::read<Person>("");
 
 	}
 }
